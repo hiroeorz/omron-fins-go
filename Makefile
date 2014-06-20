2a402312cd7ef6492c13b1e1df02aa72d9c8dc64
Shin Hiroe <hiroe.orz@gmail.com>
2014-06-20 21:18:11 +0900

    add Makefile and example

---
+++ Makefile
@@ -0,0 +1,21 @@
+.PHONY: test
+
+GO=go
+PACKAGE=github.com/hiroeorz/fins
+GOOS=linux
+GOARCH=arm
+GOARM=5
+
+all: build
+
+build:
+	${GO} install ${PACKAGE}
+
+arm:
+	GOOS=${GOOS} GOARCH=${GOARCH} GOARM=${GOARM} ${GO} install ${PACKAGE}
+
+clean:
+	rm -f ${GOPATH}/pkg/*/${PACKAGE}.a
+
+fmt:
+	go fmt ./...
---
+++ example/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hiroeorz/fins"
+	"log"
+)
+
+func main() {
+	srcAddr := "192.168.10.30:9600"
+	dstAddr := "192.168.10.6:9600"
+	listenChan := fins.Listen(srcAddr)
+
+	vals, err := fins.ReadDM(listenChan, srcAddr, dstAddr, 100, 10)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(vals)
+}
